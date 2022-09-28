40a88ab162234e3806ea0f102415fb68280934db
tpiechowiak <szuedat06@gmail.com>
2022-09-28 03:05:45 +0200

    Added more meta tags to improve SEO.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,6 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>ZSK TV Poznań</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="theme-color" content="#272d70" />
       </Head>
       <Component {...pageProps} />
     </>
